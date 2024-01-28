d4a2b35d53806a2fa17688d5d193b88efe60a5ae
Jean-François Molderez <jfmolderez@hotmail.com>
2024-01-28 12:35:06 +0000

    Updates in vector-fruit-salad

--- vector-fruit-salad/src/main.rs
+++ vector-fruit-salad/src/main.rs
@@ -6,9 +6,10 @@ useful data structures in Rust. A vector is represented using the Vec<T> type.
 
 use rand::seq::SliceRandom; // for randomizing the order of the fruit
 use rand::thread_rng; // for generating random numbers
+use std::io;
 
-fn main() {
-    let mut fruit = vec![
+fn init_fruit() -> Vec<String> {
+    let refs = vec![
         "Apple",
         "Orange",
         "Banana",
@@ -18,15 +19,62 @@ fn main() {
         "Pomegranate",
         "Cherry",
         "Peach",
+        "Pear",
+        "Strawberry",
+        "Kiwi",
+        "Grape",
+        "Plum",
+        "Passion Fruit",
+        "Lemon",
+        "Lime",
+        "Coconut",
+        "Watermelon",
+        "Cantaloupe",
+        "Nut",
+        "Peanut",
+        "Cashew",
+        "Almond",
+        "Pistachio",
     ];
+    refs.iter()
+        .map(|s| s.to_string().to_ascii_lowercase())
+        .collect()
+}
 
-    // Scramble (shuffle) the fruit
-    fruit.shuffle(&mut thread_rng());
+fn make_salad(num: usize) -> Vec<String> {
+    let mut result = Vec::new();
+    let fruits = init_fruit();
+
+    for _ in 0..num {
+        println!("Please enter a fruit : ");
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+        input = input.trim().to_string().to_ascii_lowercase();
+
+        while !fruits.contains(&input) {
+            println!("Please enter a valid fruit : ");
+            input = String::new();
+            io::stdin()
+                .read_line(&mut input)
+                .expect("Failed to read line");
+            input = input.trim().to_string().to_ascii_lowercase();
+        }
+        result.push(input);
+    }
+    // Scramble (shuffle) the result
+    result.shuffle(&mut thread_rng());
+    result
+}
+
+fn main() {
+    let salad = make_salad(5);
 
     // Print out the fruit salad
     println!("Fruit Salad:");
-    for (i, item) in fruit.iter().enumerate() {
-        if i != fruit.len() - 1 {
+    for (i, item) in salad.iter().enumerate() {
+        if i != salad.len() - 1 {
             print!("{}, ", item);
         } else {
             println!("{}", item);
